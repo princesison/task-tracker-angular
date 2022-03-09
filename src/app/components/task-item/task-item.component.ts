1aa5ae785e4ae851c3c24a55b8e6b53f2119fd91
Kim <princesison305@gmail.com>
2022-03-09 10:32:03 +0800

    Modified/Create Component for Task Service

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Task } from 'src/app/interface/Task';
 
@@ -9,9 +9,20 @@ import { Task } from 'src/app/interface/Task';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
   faTimes = faTimes;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
+  }
 }
--- src/app/service/task-service.service.ts
+++ src/app/service/task-service.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Task } from '../interface/Task';
-import { TASKS } from '../mock-tasks';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskServiceService {
-  constructor() {}
+  private apiURL = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
 
   getTask(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiURL);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiURL}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+  toggleReminder(task: Task): Observable<Task> {
+    const url = `${this.apiURL}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
   }
 }
